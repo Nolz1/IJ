7bf5c5ec0b32b8f7cecb711cabae123bd6747df4
Nolz1 <139223296+Nolz1@users.noreply.github.com>
2024-01-17 09:23:14 +0100

    Add files via upload

--- javascript/work.js
+++ javascript/work.js
@@ -20,64 +20,68 @@ dropButton.addEventListener('click', (e) => {
 window.addEventListener('scroll', reveal);
 
 function reveal(){
-    var reveals1 = document.querySelectorAll(".reveal1");
 
-    for(var i = 0; i < reveals1.length; i++){
+    if(window.innerWidth > 1024){
+        
+            var reveals1 = document.querySelectorAll(".reveal1");
 
-        var windowheight = window.innerHeight;
-        var revealTop = reveals1[i].getBoundingClientRect().top;
-        var revealpoint = 100;
+        for(var i = 0; i < reveals1.length; i++){
 
-        if(revealTop < windowheight - revealpoint){
-            reveals1[i].classList.add('active');
+            var windowheight = window.innerHeight;
+            var revealTop = reveals1[i].getBoundingClientRect().top;
+            var revealpoint = 100;
+
+            if(revealTop < windowheight - revealpoint){
+                reveals1[i].classList.add('active');
+            }
+            else{
+                reveals1[i].classList.remove('active');
+            }
         }
-        else{
-            reveals1[i].classList.remove('active');
+        var reveals2 = document.querySelectorAll(".reveal2");
+
+        for(var i = 0; i < reveals2.length; i++){
+
+            var windowheight = window.innerHeight;
+            var revealTop = reveals2[i].getBoundingClientRect().top;
+            var revealpoint = 150;
+
+            if(revealTop < windowheight - revealpoint){
+                reveals2[i].classList.add('active');
+            }
+            else{
+                reveals2[i].classList.remove('active');
+            }
         }
-    }
-    var reveals2 = document.querySelectorAll(".reveal2");
+        var reveals3 = document.querySelectorAll(".reveal3");
 
-    for(var i = 0; i < reveals2.length; i++){
+        for(var i = 0; i < reveals3.length; i++){
 
-        var windowheight = window.innerHeight;
-        var revealTop = reveals2[i].getBoundingClientRect().top;
-        var revealpoint = 150;
+            var windowheight = window.innerHeight;
+            var revealTop = reveals3[i].getBoundingClientRect().top;
+            var revealpoint = 200;
 
-        if(revealTop < windowheight - revealpoint){
-            reveals2[i].classList.add('active');
+            if(revealTop < windowheight - revealpoint){
+                reveals3[i].classList.add('active');
+            }
+            else{
+                reveals3[i].classList.remove('active');
+            }
         }
-        else{
-            reveals2[i].classList.remove('active');
-        }
-    }
-    var reveals3 = document.querySelectorAll(".reveal3");
+        var reveals4 = document.querySelectorAll(".reveal4");
 
-    for(var i = 0; i < reveals3.length; i++){
+        for(var i = 0; i < reveals4.length; i++){
 
-        var windowheight = window.innerHeight;
-        var revealTop = reveals3[i].getBoundingClientRect().top;
-        var revealpoint = 200;
+            var windowheight = window.innerHeight;
+            var revealTop = reveals4[i].getBoundingClientRect().top;
+            var revealpoint = 250;
 
-        if(revealTop < windowheight - revealpoint){
-            reveals3[i].classList.add('active');
-        }
-        else{
-            reveals3[i].classList.remove('active');
-        }
-    }
-    var reveals4 = document.querySelectorAll(".reveal4");
-
-    for(var i = 0; i < reveals4.length; i++){
-
-        var windowheight = window.innerHeight;
-        var revealTop = reveals4[i].getBoundingClientRect().top;
-        var revealpoint = 250;
-
-        if(revealTop < windowheight - revealpoint){
-            reveals4[i].classList.add('active');
-        }
-        else{
-            reveals4[i].classList.remove('active');
+            if(revealTop < windowheight - revealpoint){
+                reveals4[i].classList.add('active');
+            }
+            else{
+                reveals4[i].classList.remove('active');
+            }
         }
     }
 };
